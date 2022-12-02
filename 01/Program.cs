c38c7967fd58d0516b90d69da890988ef85e0444
Слободин <slobodin.roman@gmail.com>
2022-12-02 22:55:37 +0600

    Lesson1

--- 01/Program.cs
+++ 01/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a;
+a=10;
+string? s=Console.ReadLine();
+a=Convert.ToInt32(s);
+int b=a*a;
+System.Console.WriteLine(b);
---
+++ 02/Program.cs
@@ -0,0 +1,6 @@
+﻿double a=Convert.ToDouble(Console.ReadLine());
+double b=Convert.ToDouble(Console.ReadLine());
+
+if (a*a==b) System.Console.WriteLine($"{b} является квадратом {a}");
+if (b*b==a) System.Console.WriteLine($"{a} является квадратом {b}");
+ 
