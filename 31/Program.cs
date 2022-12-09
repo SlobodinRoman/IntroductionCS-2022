0747b1ac81d59afeb7c97d38a06ec19102969679
Слободин <slobodin.roman@gmail.com>
2022-12-09 20:17:47 +0600

    Lesson3

---
+++ 31/Program.cs
@@ -0,0 +1,11 @@
+﻿// 31. Показать кубы чисел, заканчивающихся на четную цифру
+
+Random random=new Random();
+for (int i=0;i<5;i++)
+{
+    int a=random.Next(1,100);
+    int b=(int)Math.Pow(a,3);
+    if (b%10%2==0) 
+    System.Console.WriteLine($"{a}, {b}");
+}
+ 
