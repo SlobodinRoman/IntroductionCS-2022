17bf4aa93d6c764db34f0befb6df1c8c76ff5375
Слободин <slobodin.roman@gmail.com>
2022-12-09 20:16:36 +0600

    Lesson3

---
+++ 25/Program.cs
@@ -0,0 +1,10 @@
+﻿// 25. Вывести на экран кубы чисел от 1 до N
+
+int N=Convert.ToInt32(Console.ReadLine());
+int i=1;
+while (i<=N)
+{
+    i=(int)Math.Pow(i,3);
+    System.Console.Write($"{i} ");
+    i=i+1;
+}
