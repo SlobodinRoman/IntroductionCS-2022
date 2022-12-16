f2844394a7067bfa92f61fdbc28e7e4915069e30
Слободин <slobodin.roman@gmail.com>
2022-12-16 20:37:32 +0600

    Lesson6

---
+++ 66/Program.cs
@@ -0,0 +1,37 @@
+﻿// 66. Написать подпрограмму, которая в квадратной матрице чисел находит 
+//сумму элементов главной диагонали
+
+int[,] Create2DArray(int n,int m)
+{
+    int[,] a=new int[n,m];
+    for(int i=0;i<n;i++)
+         for(int j=0;j<m;j++)
+                a[i,j]=i+j;
+    return a;
+}
+
+void Print2DArray(int[,] a)
+{
+    for(int i=0;i<a.GetLength(0);i++)
+    {
+        for(int j=0;j<a.GetLength(1);j++)
+            System.Console.Write($"{a[i,j],4}");
+                System.Console.WriteLine();
+    }
+}
+
+int SumMainDiagonal(int[,] a)
+{
+    int s=0;
+    if (a.GetLength(0)!=a.GetLength(1))
+        throw new ArgumentException("Матрица не квадратная");
+
+    for(int i=0;i<a.GetLength(0);i++)
+        for(int j=0;j<a.GetLength(1);j++)
+            if (i==j) s+=a[i,j];
+    return s;
+}
+
+int[,] a=Create2DArray(4,4);
+Print2DArray(a);
+System.Console.WriteLine(SumMainDiagonal(a));
