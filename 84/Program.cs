e81df313370796b7c84cd0555ab33d5aa071508b
Слободин <slobodin.roman@gmail.com>
2022-12-22 01:30:21 +0600

    Lesson8.1

---
+++ 84/Program.cs
@@ -0,0 +1,6 @@
+﻿// 84. Определить являются ли введенные с клавиатуры символы правильно записью целого числа. Вычислить сумму цифр введенного числа
+
+string s=Console.ReadLine();
+int n;
+int.TryParse(s,out n);
+System.Console.WriteLine(n);
